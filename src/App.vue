4244006ca2af281d97d64a8629eadbc865cb54fc
eduardonwa <eduardongua@hotmail.com>
2022-03-01 16:56:56 -0700

    under construction

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,10 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-</script>
-
 <template>
-  <div>
-
+  <div class="mx-auto container bg-yellow-400">
+    <div class="flex justify-center items-center">
+      <span class="p-12 font-semibold text-4xl">Under Construction</span>
+    </div>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<script setup>
+</script>
